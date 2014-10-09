036315827e6d69176f58f12f89c8849fe71bc66f
Joaquin R <globojorro@gmail.com>
2014-10-08 23:51:01 -0500

    Guardar tareas en localStorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,21 @@
  * Controller of the tanTan4App
  */
 angular.module('tanTan4App')
-  .controller('MainCtrl', function ($scope, localStorageService) {
-    $scope.todos = [];
+    .controller('MainCtrl', function ($scope, localStorageService) {
 
-    $scope.addTodo = function () {
-        $scope.todos.push($scope.todo);
-        $scope.todo = '';
-    };
+        var todosInStore = localStorageService.get('todos');
+        $scope.todos = todosInStore && todosInStore.split('\n') || [];
 
-    $scope.removeTodo = function (index) {
-        $scope.todos.splice(index, 1);
-    };
-  });
+        $scope.$watch('todos', function () {
+            localStorageService.add('todos', $scope.todos.join('\n'));
+        }, true);
+
+        $scope.addTodo = function () {
+            $scope.todos.push($scope.todo);
+            $scope.todo = '';
+        };
+
+        $scope.removeTodo = function (index) {
+            $scope.todos.splice(index, 1);
+        };
+    });
