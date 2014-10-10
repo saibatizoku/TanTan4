bc05b45e8871e72cfc609061a203ab135cb9a302
Joaquin R <globojorro@gmail.com>
2014-10-10 10:14:30 -0500

    Conectar a CouchDB

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,13 @@
  * Controller of the tanTan4App
  */
 angular.module('tanTan4App')
-    .controller('MainCtrl', function ($scope, localStorageService) {
+    .controller('MainCtrl', function ($scope, localStorageService, cornercouch) {
+
+        $scope.server = cornercouch("http://localhost:5984", "GET");
+        $scope.server.session();
+        $scope.tt4db = $scope.server.getDB('tt4');
+        $scope.newentry = $scope.tt4db.newDoc();
+        $scope.tt4db.query("tt4", "recent-items", { include_docs: true, descending: true, limit: 8 });
 
         var todosInStore = localStorageService.get('todos');
         $scope.todos = todosInStore && todosInStore.split('\n') || [];
