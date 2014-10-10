728dea57af84d415fa8d206d403f7f2f3258bd34
Joaquin R <globojorro@gmail.com>
2014-10-10 10:19:26 -0500

    Comillas simples

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,11 +10,11 @@
 angular.module('tanTan4App')
     .controller('MainCtrl', function ($scope, localStorageService, cornercouch) {
 
-        $scope.server = cornercouch("http://localhost:5984", "GET");
+        $scope.server = cornercouch('http://localhost:5984', 'GET');
         $scope.server.session();
         $scope.tt4db = $scope.server.getDB('tt4');
         $scope.newentry = $scope.tt4db.newDoc();
-        $scope.tt4db.query("tt4", "recent-items", { include_docs: true, descending: true, limit: 8 });
+        $scope.tt4db.query("tt4", "recent-items", { 'include_docs': true, descending: true, limit: 8 });
 
         var todosInStore = localStorageService.get('todos');
         $scope.todos = todosInStore && todosInStore.split('\n') || [];
